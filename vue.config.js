a8f744a045e48dcdb9148f54e0adecebf069805f
khush <khushnoodhussain1111@gmail.com>
2021-03-29 16:24:09 +0530

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  devServer: {
+    proxy: 'http://notifycus-env.eba-mxaatkn4.ap-south-1.elasticbeanstalk.com/'
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
+
+ 
+
+
+
+    
